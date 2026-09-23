bd9d0522483b19bc4d1a49bc877681a8d87b35d7
Calvin Bonner <me@calvinbonner.com>
2026-09-23 16:53:46 -0400

    fix: remove unused export for 'md' helper in index.ts

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -8,6 +8,5 @@ export * from "./each";
 export * from "./error";
 export * from "./html";
 export * from "./is-htmx";
-export * from "./md";
 export * from "./redirect-to";
 export * from "./repeat";
